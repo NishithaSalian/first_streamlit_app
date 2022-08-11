555764312672966c4d6a3ed5ec217ad974299643
Nishitha Salian <110963400+NishithaSalian@users.noreply.github.com>
2022-08-11 19:56:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,6 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
    else:
-#streamlit.write('The user entered ', fruit_choice)
      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
 # takes the json version of response and normalise it
